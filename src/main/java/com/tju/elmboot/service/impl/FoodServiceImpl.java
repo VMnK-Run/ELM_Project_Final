f2d3e54ca6c1e49419ab618a7b844a2cbddd6a7b
VMnK-Run <wang232519@tju.edu.cn>
2022-09-25 19:52:26 +0800

    添加生成商品组合

--- src/main/java/com/tju/elmboot/service/impl/FoodServiceImpl.java
+++ src/main/java/com/tju/elmboot/service/impl/FoodServiceImpl.java
@@ -3,6 +3,7 @@ package com.tju.elmboot.service.impl;
 import com.tju.elmboot.mapper.BusinessMapper;
 import com.tju.elmboot.mapper.CommentMapper;
 import com.tju.elmboot.mapper.FoodMapper;
+import com.tju.elmboot.po.Business;
 import com.tju.elmboot.po.Comment;
 import com.tju.elmboot.po.Food;
 import com.tju.elmboot.service.FoodService;
@@ -35,20 +36,7 @@ public class FoodServiceImpl implements FoodService {
         Map<Food, Double> foodMap = new HashMap<>();
         double maxStar = -0.1;
         for (Food food : foodList) {
-            Comment comment = new Comment();
-            comment.setFoodId(food.getFoodId());
-            List<Comment> commentList = commentMapper.listComment(comment);
-            double averageStar = 0;
-            if (!commentList.isEmpty()) {
-                int num = commentList.size();
-                long totalStar = 0;
-                for (Comment c : commentList) {
-                    totalStar += c.getStar();
-                }
-                averageStar = (double) (totalStar) / num;
-            } else {
-                averageStar = 5.0;
-            }
+            double averageStar = this.getAverageStarOfFood(food);
             if(averageStar >= maxStar) {
                 maxStar = averageStar;
                 foodMap.put(food, averageStar);
@@ -79,15 +67,73 @@ public class FoodServiceImpl implements FoodService {
         int hour = Integer.parseInt(currentTime.substring(11, 13));
         List<Integer> candidateTypes = new ArrayList<>();
         if(5 <= hour && hour <= 10) { // 早上
-
+            candidateTypes.add(2);
+            candidateTypes.add(7);
+            candidateTypes.add(9);
         } else if (hour <= 15) { // 中午
-
+            candidateTypes.add(1);
+            candidateTypes.add(4);
+            candidateTypes.add(5);
+            candidateTypes.add(8);
+            candidateTypes.add(9);
+            candidateTypes.add(10);
         } else { // 晚上
-
+            candidateTypes.add(1);
+            candidateTypes.add(5);
+            candidateTypes.add(6);
+            candidateTypes.add(7);
         }
-//        for (int typeID : candidateTypes) {
-//            List<Business> businessList =
-//        }
+
+        Map<Food, Double> map = new HashMap<>();
+
+        for (int typeID : candidateTypes) {
+            List<Business> businessList = businessMapper.listBusinessByOrderTypeId(typeID);
+            for (Business business : businessList) {
+                List<Food> foodList = foodMapper.listFoodByBusinessId(business.getBusinessId());
+                for (Food food : foodList) {
+                    double averageStar = this.getAverageStarOfFood(food);
+                    map.put(food, averageStar);
+                }
+            }
+        }
+        List<Map.Entry<Food, Double>> list = new ArrayList<>(map.entrySet());
+        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        Set<Integer> typeSet = new HashSet<>();
+        int num = 0;
+        int randomNum = CommonUtil.getRandomNumber(hour);
+        for(Map.Entry<Food, Double> entry : list) {
+            Food food = entry.getKey();
+            Business business = businessMapper.getBusinessById(food.getBusinessId());
+            if(!typeSet.contains(business.getOrderTypeId())) {
+                typeSet.add(business.getOrderTypeId());
+                res.add(food);
+                num++;
+            }
+            if(num == randomNum) {
+                break;
+            }
+        }
+
         return res;
+    }
+
+
+    private Double getAverageStarOfFood(Food food) {
+        Comment comment = new Comment();
+        comment.setFoodId(food.getFoodId());
+        List<Comment> commentList = commentMapper.listComment(comment);
+        double averageStar = 0;
+        if (!commentList.isEmpty()) {
+            int num = commentList.size();
+            long totalStar = 0;
+            for (Comment c : commentList) {
+                totalStar += c.getStar();
+            }
+            averageStar = (double) (totalStar) / num;
+        } else {
+            averageStar = 5.0;
+        }
+        return averageStar;
     }
 }
