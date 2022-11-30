4913cb82dea381f99d4ac5d5e4fb2fb09ac490a4
Aukira <lixinran1826@163.com>
2022-11-30 11:46:49 +0800

    add account

--- src/main/java/com/tju/elmboot/po/TransactionEntity.java
+++ src/main/java/com/tju/elmboot/po/TransactionEntity.java
@@ -2,12 +2,10 @@ package com.tju.elmboot.po;
 
 public class TransactionEntity extends VirtualWalletTransaction{
     public int transactionType;
-    @Override
     public int getTransactionType() {
         return transactionType;
     }
 
-    @Override
     public void setTransactionType(int transactionType) {
         this.transactionType = transactionType;
     }
